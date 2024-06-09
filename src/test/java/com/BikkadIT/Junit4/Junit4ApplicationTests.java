27c547aadbbd856381ae95908513a700092e2edc
mohinigai <you@example.com>
2024-06-09 21:56:37 +0530

    v1:B/12345, first maven

---
+++ src/test/java/com/BikkadIT/Junit4/Junit4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.Junit4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
